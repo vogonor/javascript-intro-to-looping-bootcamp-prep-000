3f5faa8795314eacc3ffde60a5ede94b8fdf00f4
observant-algorithm-6319 <vogonor@yahoo.com>
2020-09-16 15:26:32 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,16 @@
+function forLoop(array){
+  array = [ ]
+  var iValue = ""
+  for(let i = 0; i < 25; i++) {
+    if(i===0 || i === 1) {
+        iValue = `"I am ${i} strange loop."`
+         this.array[i].push(iValue)
+    }else {
+      iValue = `"I am ${i} strange loops."`
+      this.array[i].push(iValue)
+    }
+  }  
+    return array
+  }
+  
+//TypeError: Cannot read property '0' of undefined
